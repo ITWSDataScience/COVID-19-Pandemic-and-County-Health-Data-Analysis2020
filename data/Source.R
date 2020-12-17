a71875ec68c69c851128f3e22b81f52fb4e4aa27
Mouhongkiii <mouhongxi@gmail.com>
2020-12-17 01:08:25 +0000

    update population source

--- data/Source.R
+++ data/Source.R
@@ -18,6 +18,7 @@ final.determinants <- append(final.determinants$Code, "county_fips", after = 0)
 
 # Load all data
 chr.data.2019 <- readRDS("data/chr.data.2019.rds")
+popu <- subset(chr.data.2019, select = c(county_fips, population))
 chr.data.2019 <- chr.data.2019 %>%
   as_data_frame %>%
   select(final.determinants)
@@ -79,9 +80,6 @@ kendall.func <- function(x.data, sd.data) {
 
 date_of_study = "2020-11-15"
 
-# use population data from 2020CHR for calculation of the COVID-19 death rate
-chr <- read_csv("data/2020CHR.csv")
-popu <- subset(chr, select = c(FIPS, Population))
 
 # Historical data
 covid_hist = read.csv(text=getURL(paste0("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-22-2020.csv")))
@@ -89,11 +87,11 @@ covid_us_hist = subset(covid_hist, Country_Region == "US" & is.na(FIPS)==F)
 covid_us_hist = subset(covid_us_hist, select = c(FIPS, Deaths))
 covid_us_hist$FIPS <- str_pad(covid_us_hist$FIPS, 5, pad = "0")
 covid_us_hist = merge(x = covid_us_hist,y = geo, by.x = "FIPS", by.y = "county_fips", all.y = TRUE)
-covid_us_hist = merge(x = covid_us_hist,y = popu, by = "FIPS", all.x = TRUE)
-covid_us_hist$period = "03-22-2020"
+covid_us_hist = merge(x = covid_us_hist,y = popu, by.x = "FIPS", by.y = "county_fips", all.x = TRUE)
+covid_us_hist$period = "04-14-2020"
 
 # collect data from March to Month of study on 23rd of each month
-date_of_all = format(seq(as.Date("2020-03-23"), as.Date(strptime(date_of_study,"%Y-%m-%d")), by = "month"),"%m-%d-%Y")
+date_of_all = format(seq(as.Date("2020-04-15"), as.Date(strptime(date_of_study,"%Y-%m-%d")), by = "month"),"%m-%d-%Y")
 
 for (i in 1:length(date_of_all)){
   covid_daily = read.csv(text=getURL(paste0("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/",date_of_all[i],".csv")))
@@ -101,13 +99,13 @@ for (i in 1:length(date_of_all)){
   covid_daily = subset(covid_daily, select = c(FIPS, Deaths))
   covid_daily$FIPS <- str_pad(covid_daily$FIPS, 5, pad = "0")
   covid_daily = merge(x = covid_daily,y = geo, by.x = "FIPS", by.y = "county_fips", all.y = TRUE)
-  covid_daily = merge(x = covid_daily,y = popu, by = "FIPS", all.x = TRUE)
+  covid_daily = merge(x = covid_daily,y = popu, by.x = "FIPS", by.y = "county_fips", all.x = TRUE)
   covid_daily$period = date_of_all[i]
   covid_us_hist <- rbind(covid_us_hist, covid_daily)
 }
 
 # calculation of death rate
-covid_us_hist <- dplyr::rename(covid_us_hist, c(county_fips = FIPS, death_num = Deaths, population = Population))
+covid_us_hist <- dplyr::rename(covid_us_hist, c(county_fips = FIPS, death_num = Deaths))
 covid_us_hist$death_rate <- covid_us_hist$death_num/covid_us_hist$population*100000
 covid.data <- covid_us_hist %>% drop_na(death_num,population)
 
@@ -120,9 +118,9 @@ county_hospitals_aggregated = hospitals %>%
   group_by(COUNTYFIPS) %>%
   dplyr::summarise(beds = sum(BEDS, na.rm=TRUE))
 county_hospitals_aggregated$county_fips = str_pad(county_hospitals_aggregated$COUNTYFIPS, 5, pad = "0")
-county_hospitals_aggregated <- merge(county_hospitals_aggregated,popu,by.x = "county_fips", by.y = "FIPS", all = T)
-county_hospitals_aggregated <- county_hospitals_aggregated %>% drop_na(Population, beds)
-county_hospitals_aggregated$popu_beds = county_hospitals_aggregated$beds/county_hospitals_aggregated$Population*1000
+county_hospitals_aggregated <- merge(county_hospitals_aggregated,popu,by = "county_fips", all = T)
+county_hospitals_aggregated <- county_hospitals_aggregated %>% drop_na(population, beds)
+county_hospitals_aggregated$popu_beds = county_hospitals_aggregated$beds/county_hospitals_aggregated$population*1000
 county_hospitals_aggregated <- subset(county_hospitals_aggregated, select = c(county_fips, popu_beds))
 
 factors = merge(county_hospitals_aggregated, chr.data.2019, by = "county_fips", all = T)
